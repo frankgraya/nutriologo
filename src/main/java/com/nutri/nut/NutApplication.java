d9f484f7ea1f11c96b29b47cb487871a3787d4c8
Frank <francisco.graya26@gmail.com>
2023-03-21 13:46:03 -0600

    first commit

---
+++ src/main/java/com/nutri/nut/NutApplication.java
@@ -0,0 +1,13 @@
+package com.nutri.nut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nutri/nut/NutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutri.nut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
